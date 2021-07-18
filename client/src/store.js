692fbc35cf420b63d8e256ffbd572f4b0e97ca52
render07 <59060987+render07@users.noreply.github.com>
2021-07-18 18:07:25 +0800

    added comment in storejs

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,10 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
+    //window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 ));
 
 export default store;
